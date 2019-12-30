96ff3c145381853160a83f1917041be5a58a63e6
Luis Herrera <herrera.lrh@gmail.com>
2019-12-30 16:43:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,10 @@
 class Transfer
+  attr_reader :sender, :receiver, :amount
   
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = 'pending'
+  end
 end
